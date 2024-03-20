a0614aa2659bc6e1fead7ea10641f09ac9c9e223
Virginia Álvarez <virginiaalvarez.ar@gmail.com>
2024-03-20 16:10:25 +0100

    add css and js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const container= document.querySelector('.container');
+const input = document.querySelector('.input');
+const btn= document.querySelector('.btn');
+
+btn.addEventListener('click',()=>{
+    container.classList.toggle('active');
+    input.focus();
+})
